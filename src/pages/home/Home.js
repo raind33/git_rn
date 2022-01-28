db8736019a82936a1614d32ee87526ddd11b517f
wuyl <2331336537@qq.com>
2022-01-28 23:43:19 +0800

    feat: store

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,27 @@
 import React, { memo } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
-export default memo(function Home() {
+const Tab = createMaterialTopTabNavigator()
+function Tab1() {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>tab1</Text>
     </View>
+  )
+}
+function Tab2() {
+  return (
+    <View>
+      <Text>tab2</Text>
+    </View>
+  )
+}
+export default memo(function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="tab1" component={Tab1} />
+      <Tab.Screen name="tab2" component={Tab2} />
+    </Tab.Navigator>
   )
 })
